2b8e45da4f9134a31b8b6dd5c8df4e4ec5133054
Djolic98 <andjeladjolic1@gmail.com>
2021-06-26 22:06:22 +0200

    proba

--- src/app/pretraga/food/food.page.ts
+++ src/app/pretraga/food/food.page.ts
@@ -7,7 +7,7 @@ import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-food',
-  templateUrl: './food.page.html',
+  templateUrl: './food.page.html', 
   styleUrls: ['./food.page.scss'],
 })
 export class FoodPage implements OnInit {
